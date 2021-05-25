c1a334a0f8c3d5613befcbfca0365c7fc54847c7
Yusuf Berk Sonmez <yberksonmez@gmail.com>
2021-05-25 18:23:42 -0500

    Things we have done so far

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,3 +1,9 @@
 public class BranchingPractice {
-
+    public static void main(String[] args) {
+        //create new project
+        // enabled version control
+        // added.gitignore with .idea, target folder and .iml file
+        // create initial commit by keeping track of all files
+        // shared project on github
+    }
 }
